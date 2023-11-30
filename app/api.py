349f68c90a534f0c8cbc91edf43c9628d83b3047
Valimp <valentinchauvet.contact@gmail.com>
2023-11-30 15:17:45 +0100

    add middleware to raise HTTPExceptions

--- app/api.py
+++ app/api.py
@@ -17,7 +17,6 @@ from app.utils import init_sentry
 
 logger = get_logger(level=settings.log_level.to_int())
 
-
 app = FastAPI(
     title="nutripatrol",
     contact={
@@ -47,11 +46,27 @@ def robots_txt():
     return """User-agent: *\nDisallow: /"""
 
 
+@app.middleware("http")
+async def catch_exceptions(request: Request, call_next):
+    try:
+        return await call_next(request)
+    except DoesNotExist:
+        raise HTTPException(status_code=404, detail="Not found")
+    except Exception as e:
+        logger.exception(e)
+        raise HTTPException(status_code=500, detail="Internal server error")
+
+
+class TicketStatus(str, Enum):
+    open = "open"
+    closed = "closed"
+
+
 class TicketCreate(BaseModel):
     barcode: str = Field(..., description="Barcode of the product")
     type: str = Field(..., description="Type of the issue")
     url: str = Field(..., description="URL of the product")
-    status: str = Field(..., description="Status of the ticket")
+    status: TicketStatus = Field(..., description="Status of the ticket")
     image_id: str = Field(..., description="Image ID of the product")
     flavour: Flavor = Field(..., description="Flavour of the product")
     created_at: datetime = Field(default_factory=datetime.utcnow)
@@ -68,7 +83,10 @@ class FlagCreate(BaseModel):
     user_id: str = Field(..., description="User ID of the flagger")
     device_id: str = Field(..., description="Device ID of the flagger")
     source: str = Field(..., description="Source of the flag")
-    confidence: float = Field(..., description="Confidence of the flag")
+    confidence: float = Field(
+        ...,
+        description="Confidence of the flag, it's a machine learning confidence score. It's a float between 0 and 1 and it's optional.",
+    )
     image_id: str = Field(..., description="Image ID of the product")
     flavour: Flavor = Field(..., description="Flavour of the product")
     reason: str = Field(..., description="Reason of the flag")
@@ -85,57 +103,46 @@ class Flag(FlagCreate):
 @app.post("/flags")
 def create_flag(flag: FlagCreate) -> Flag:
     with db:
-        try:
-            # Search for existing ticket
-            # With the same barcode, url, type and flavour
-            ticket = TicketModel.get_or_none(
-                TicketModel.barcode == flag.barcode,
-                TicketModel.url == flag.url,
-                TicketModel.type == flag.type,
-                TicketModel.flavour == flag.flavour,
+        # Search for existing ticket
+        # With the same barcode, url, type and flavour
+        ticket = TicketModel.get_or_none(
+            TicketModel.barcode == flag.barcode,
+            TicketModel.url == flag.url,
+            TicketModel.type == flag.type,
+            TicketModel.flavour == flag.flavour,
+        )
+        # If no ticket found, create a new one
+        if ticket is None:
+            newTicket = TicketCreate(
+                barcode=flag.barcode,
+                url=flag.url,
+                type=flag.type,
+                flavour=flag.flavour,
+                status="open",
+                image_id=flag.image_id,
             )
-            # If no ticket found, create a new one
-            if ticket is None:
-                newTicket = TicketCreate(
-                    barcode=flag.barcode,
-                    url=flag.url,
-                    type=flag.type,
-                    flavour=flag.flavour,
-                    status="open",
-                    image_id=flag.image_id,
-                )
-                ticket = _create_ticket(newTicket)
-            new_flag = FlagModel.create(**flag.model_dump())
-            # Associate the flag with the ticket
-            new_flag.ticket_id = ticket.id
-            new_flag.save()
-            return new_flag
-        except Exception as error:
-            raise HTTPException(status_code=500, detail=f"{error}")
+            ticket = _create_ticket(newTicket)
+        new_flag = FlagModel.create(**flag.model_dump())
+        # Associate the flag with the ticket
+        new_flag.ticket_id = ticket.id
+        new_flag.save()
+        return new_flag
 
 
 # Get all flags (one to many relationship)
 @app.get("/flags")
 def get_flags():
     with db:
-        try:
-            flags = FlagModel.select()
-            return [model_to_dict(flag) for flag in flags]
-        except Exception as error:
-            raise HTTPException(status_code=500, detail=f"{error}")
+        flags = FlagModel.select()
+        return [model_to_dict(flag) for flag in flags]
 
 
 # Get flag by ID (one to one relationship)
 @app.get("/flags/{flag_id}")
 def get_flag(flag_id: int):
     with db:
-        try:
-            flag = FlagModel.get_by_id(flag_id)
-            return flag
-        except DoesNotExist:
-            raise HTTPException(status_code=404, detail="Flag not found")
-        except Exception as error:
-            raise HTTPException(status_code=500, detail=f"{error}")
+        flag = FlagModel.get_by_id(flag_id)
+        return flag
 
 
 def _create_ticket(ticket: TicketCreate):
@@ -153,52 +160,31 @@ def create_ticket(ticket: TicketCreate) -> Ticket:
 @app.get("/tickets")
 def get_tickets():
     with db:
-        try:
-            tickets = TicketModel.select()
-            return [model_to_dict(ticket) for ticket in tickets]
-        except Exception as error:
-            raise HTTPException(status_code=500, detail=f"{error}")
+        tickets = TicketModel.select()
+        return [model_to_dict(ticket) for ticket in tickets]
 
 
 # Get ticket by id (one to one relationship)
 @app.get("/tickets/{ticket_id}")
 def get_ticket(ticket_id: int):
     with db:
-        try:
-            ticket = TicketModel.get_by_id(ticket_id)
-            return ticket
-        except DoesNotExist:
-            raise HTTPException(status_code=404, detail="Flag not found")
-        except Exception as error:
-            raise HTTPException(status_code=500, detail=f"{error}")
+        ticket = TicketModel.get_by_id(ticket_id)
+        return ticket
 
 
 # Get all flags for a ticket by id (one to many relationship)
 @app.get("/tickets/{ticket_id}/flags")
 def get_flags_by_ticket(ticket_id: int):
     with db:
-        try:
-            flags = FlagModel.select().where(FlagModel.ticket_id == ticket_id)
-            return [model_to_dict(flag) for flag in flags]
-        except Exception as error:
-            raise HTTPException(status_code=500, detail=f"{error}")
+        flags = FlagModel.select().where(FlagModel.ticket_id == ticket_id)
+        return [model_to_dict(flag) for flag in flags]
 
 
 # Update ticket status by id with enum : open, closed (soft delete)
-class TicketStatus(str, Enum):
-    open = "open"
-    closed = "closed"
-
-
 @app.put("/tickets/{ticket_id}/status")
 def update_ticket_status(ticket_id: int, status: TicketStatus):
     with db:
-        try:
-            ticket = TicketModel.get_by_id(ticket_id)
-            ticket.status = status
-            ticket.save()
-            return {"message": f"Ticket with ID {ticket_id} has been updated"}
-        except DoesNotExist:
-            raise HTTPException(status_code=404, detail="Ticket not found")
-        except Exception as error:
-            raise HTTPException(status_code=500, detail=f"{error}")
+        ticket = TicketModel.get_by_id(ticket_id)
+        ticket.status = status
+        ticket.save()
+        return {"message": f"Ticket with ID {ticket_id} has been updated"}
