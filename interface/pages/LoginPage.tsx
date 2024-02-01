cbcf2b7db8de78d7ec776bdb67527c268390d888
Valimp <valentinchauvet.contact@gmail.com>
2024-02-01 08:51:45 +0100

    fix: Center content login page

--- interface/pages/LoginPage.tsx
+++ interface/pages/LoginPage.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 export default function LoginPage() {
     return (
-        <div style={{margin: '5rem 0', color: '#281900', display: 'flex',flexDirection: "column", alignItems: "center", justifyContent:"center"}}>
+        <div style={{position: "absolute", width: "100vw", height: "100vh", zIndex: "-10", color: '#281900', display: 'flex',flexDirection: "column", alignItems: "center", justifyContent:"center"}}>
             <h2 style={{fontSize: '1.4rem', margin: "2rem 0"}}>🇫🇷 Connectez-vous avec votre compte OpenFoodFacts </h2>
             <h2 style={{fontSize: '1.4rem', margin: "2rem 0"}}>🇺🇸 / 🇬🇧 Login with your OpenFoodFacts account </h2>
             <Button 
