737dd14879fcfd673a3856fe8af55db3c1291879
Valimp <valentinchauvet.contact@gmail.com>
2025-04-18 14:55:15 +0200

    fix: build auth url using current domain

--- app/middleware/auth.py
+++ app/middleware/auth.py
@@ -1,6 +1,5 @@
 from fastapi import Depends, Request, HTTPException
 import httpx
-import os
 from enum import StrEnum, auto
 
 
@@ -9,9 +8,22 @@ class UserStatus(StrEnum):
     isLoggedIn = auto()
 
 
-PO_AUTH_ROUTE = os.getenv("PO_AUTH_ROUTE")
-if PO_AUTH_ROUTE is None:
-    raise RuntimeError("PO_AUTH_ROUTE environment variable is not set.")
+def get_auth_server(request: Request):
+    """
+    Get auth server URL from request
+
+    We deduce it by changing part of the request base URL
+    according to FOLKSONOMY_PREFIX and AUTH_PREFIX settings
+    """
+    # For dev purposes, we can use a static auth server with AUTH_SERVER_STATIC
+    # which can be specified in local_settings.py
+    auth_server_static = globals().get("AUTH_SERVER_STATIC")
+    if auth_server_static:
+        return auth_server_static
+    base_url = f"{request.base_url.scheme}://{request.base_url.netloc}"
+    # remove folksonomy prefix and add AUTH prefix
+    base_url = base_url.replace("nutripatrol" or "", "world" or "")
+    return base_url
 
 
 def get_auth_dependency(user_status: UserStatus):
@@ -22,6 +34,7 @@ def get_auth_dependency(user_status: UserStatus):
 
 async def auth_dependency(request: Request, user_status: UserStatus):
     session_cookie = request.cookies.get("session")
+    auth_base_url = get_auth_server(request) + "/cgi/auth.pl"
 
     if not session_cookie:
         raise HTTPException(status_code=401, detail="Missing session token")
@@ -29,7 +42,7 @@ async def auth_dependency(request: Request, user_status: UserStatus):
     if user_status not in UserStatus:
         raise HTTPException(status_code=400, detail=f"Invalid user status : {user_status}")
 
-    user_data = await get_user_data(session_cookie)
+    user_data = await get_user_data(session_cookie, auth_base_url)
 
     if user_status == UserStatus.isModerator:
         if user_data.get("moderator") != 1:
@@ -40,10 +53,10 @@ async def auth_dependency(request: Request, user_status: UserStatus):
             raise HTTPException(status_code=403, detail="User is not logged in")
 
 
-async def get_user_data(session_cookie: str) -> dict:
+async def get_user_data(session_cookie: str, auth_base_url: str) -> dict:
     async with httpx.AsyncClient() as client:
         response = await client.get(
-            PO_AUTH_ROUTE,
+            auth_base_url,
             cookies={"session": session_cookie},
             params={"body": "1"}
         )
