c7ea7378bff215d030e3b4da76e4939fd1e82984
Valimp <valentinchauvet.contact@gmail.com>
2025-04-24 15:06:17 +0200

    fix: use decorator for cache

--- app/middleware/auth.py
+++ app/middleware/auth.py
@@ -1,9 +1,10 @@
 import asyncio
 from enum import StrEnum, auto
+from fastapi_cache.decorator import cache
 
+import hashlib
 import httpx
 from fastapi import Depends, HTTPException, Request
-from fastapi_cache import FastAPICache
 
 import os
 
@@ -13,20 +14,28 @@ class UserStatus(StrEnum):
     isLoggedIn = auto()
 
 
+def generate_cache_key(*args, **kwargs) -> str:
+    # Expecting session_cookie at position 0, auth_base_url at position 1
+    try:
+        session_cookie = args[0]
+        auth_base_url = args[1]
+    except IndexError:
+        raise ValueError("Missing required parameters for cache key (positional args)")
+
+    key_raw = f"{auth_base_url}:{session_cookie}"
+    return "user-data:" + hashlib.md5(key_raw.encode()).hexdigest()
+
+
 def get_auth_server(request: Request):
     """
     Get auth server URL from request
-
-    We deduce it by changing part of the request base URL
-    according to FOLKSONOMY_PREFIX and AUTH_PREFIX settings
     """
     # For dev purposes, we can use a static auth server with AUTH_SERVER_STATIC
-    # which can be specified in local_settings.py
     auth_server_static = os.getenv("AUTH_SERVER_STATIC")
     if auth_server_static and auth_server_static != "":
         return auth_server_static
     base_url = f"{request.base_url.scheme}://{request.base_url.netloc}"
-    # remove folksonomy prefix and add AUTH prefix
+    # remove nutripatrol prefix and add AUTH prefix
     base_url = base_url.replace("nutripatrol" or "", "world" or "")
     return base_url
 
@@ -61,14 +70,12 @@ async def auth_dependency(request: Request, user_status: UserStatus):
             raise HTTPException(status_code=403, detail="User is not logged in")
 
 
+@cache(
+    key_builder=generate_cache_key,
+    namespace="user-data",
+    expire=60 * 60 * 24,
+)
 async def get_user_data(session_cookie: str, auth_base_url: str) -> dict:
-    cache = FastAPICache.get_backend()
-    cache_key = f"user-data:{session_cookie}"
-
-    cached_data = await cache.get(cache_key)
-    if cached_data:
-        return cached_data
-
     async with httpx.AsyncClient() as client:
         response = await client.get(
             auth_base_url, cookies={"session": session_cookie}, params={"body": "1"}
@@ -79,9 +86,4 @@ async def get_user_data(session_cookie: str, auth_base_url: str) -> dict:
         await asyncio.sleep(2)
         raise HTTPException(status_code=401, detail="Invalid session token")
 
-    user = response.json().get("user", {})
-
-    if user:
-        await cache.set(cache_key, user, expire=3600)  # Cache for 1 hour
-
-    return user
+    return response.json().get("user", {})
